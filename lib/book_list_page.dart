a25634c014d1b47040c7977ad01583b0793b6c40
yuula <topkashidog@gmail.com>
2020-08-15 07:22:14 +0900

    firestoreのFirebaseに値を追加する（try catchの使い方も）

--- lib/book_list_page.dart
+++ lib/book_list_page.dart
@@ -1,6 +1,6 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:suika/add_book/add_book_page.dart';
 import 'package:suika/book_list_model.dart';
 
 class BookListPage extends StatelessWidget {
@@ -27,6 +27,23 @@ class BookListPage extends StatelessWidget {
             );
           },
         ),
+        floatingActionButton:
+            Consumer<BookListModel>(builder: (context, model, child) {
+          return FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () async {
+              //todo
+              await Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => AddBookPage(),
+                  fullscreenDialog: true,
+                ),
+              );
+              model.fetchBooks();
+            },
+          );
+        }),
       ),
     );
   }
